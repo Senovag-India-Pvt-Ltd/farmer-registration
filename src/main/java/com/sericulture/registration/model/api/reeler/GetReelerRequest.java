2390f54f92679168eddad5e4f610ee135efa168a
Akshay Kumar P B <48916496+NAN35@users.noreply.github.com>
2024-03-12 19:51:21 +0530

    Reeler getbymobileorreelernumber

--- src/main/java/com/sericulture/registration/model/api/reeler/GetReelerRequest.java
+++ src/main/java/com/sericulture/registration/model/api/reeler/GetReelerRequest.java
@@ -2,6 +2,7 @@ package com.sericulture.registration.model.api.reeler;
 
 import com.sericulture.registration.model.api.RequestBody;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 import java.util.Date;
@@ -14,4 +15,13 @@ import java.util.Date;
 public class GetReelerRequest extends RequestBody {
     @Schema(name = "reelerId", example = "1")
     Long reelerId;
+
+    @Pattern(regexp = "^[a-zA-Z0-9\\s]*$", message = "Reeler number must contain only letters and numbers")
+    @Schema(name = "reelerNumber", example = "2")
+    String reelerNumber;
+
+    @Pattern(regexp = "^[+0-9\\s]*$", message = "Mobile number must contain only numbers")
+    @Schema(name = "mobileNumber", example = "7788990066")
+    String mobileNumber;
+
 }
